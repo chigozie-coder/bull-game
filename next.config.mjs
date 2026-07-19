bf1fb7e8d9eff62dac78b7909c80b8525ec0bf08
Chigozie Anyaeji <78688071+chigozie-coder@users.noreply.github.com>
2026-07-19 03:01:16 +0100

    Use Vercel-compatible Next configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'standalone', distDir: 'dist' };
+const nextConfig = process.env.VERCEL === '1'
+  ? {}
+  : { output: 'standalone', distDir: 'dist' };
 export default nextConfig;
